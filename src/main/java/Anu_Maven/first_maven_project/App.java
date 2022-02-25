1bae4f8211615d04391db1c11ecec83825865887
dev01 <anupama@yahoo.com>
2022-02-25 21:06:26 +0000

    the first commit in maven project

---
+++ src/main/java/Anu_Maven/first_maven_project/App.java
@@ -0,0 +1,13 @@
+package Anu_Maven.first_maven_project;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
